6df10b3158ee2af3e544c6e8303799437cb918b2
jin4 <kd1192061@st.kobedenshi.ac.jp>
2018-06-02 20:19:27 +0900

    [add] Existing project

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    "extends": "airbnb",
+    "env":{
+      "browser": true
+    },
+    rules: {
+      "linebreak-style": ["error", "windows"],
+      "prefer-destructuring" :   [ "error" ,   { "object" :   false ,   "array" :   false }],
+
+      //'('後の改行に関してのルール
+      'function-paren-newline': 'off',
+    }
+};
---
+++ README.md
@@ -0,0 +1,14 @@
+# HotelSearch
+
+指定した地域の周辺のホテルを検索するWebサイト
+
+## 詳細
+Reactの学習に簡易なWebサイトを作成する。  
+今回はGoogle Geocoding APIと楽天トラベル施設検索APIを使用。  
+指定した地点を中心とした地図を表示し、その地点から半径1kmにあるホテルの情報を表示する。
+
+## install
+`$ yarn install`
+
+## Start-up
+`$ yarn run start`
